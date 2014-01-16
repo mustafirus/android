c6c861fbb9aa43303d9e655b29abb990584cffd2
golubev <7886143+mustafirus@users.noreply.github.com>
2014-01-16 22:33:08 +0000

    1

--- ServerTroll/src/ua/net/nt/servertroll/MainActivity.java
+++ ServerTroll/src/ua/net/nt/servertroll/MainActivity.java
@@ -6,14 +6,21 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.database.Cursor;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.app.LoaderManager;
+import android.content.Loader;
 
-public class MainActivity extends Activity {
+//import android.support.v4.app.LoaderManager.LoaderCallbacks;
+
+public class MainActivity extends Activity	
+implements LoaderManager.LoaderCallbacks<List<String>> {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +33,14 @@ public class MainActivity extends Activity {
 		        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
 		        "Android", "iPhone", "WindowsMobile" };
 
-		    final StableArrayAdapter adapter = new StableArrayAdapter(this,
+		    final MyArrayAdapter adapter = new MyArrayAdapter(this,
 		        R.layout.my_list_item);
+		    
 		    listview.setAdapter(adapter);
+		    
+		    getLoaderManager().initLoader(0, null, this);
 
-		    new sshexec().execute(adapter);
+
 	}
 
 	@Override
@@ -42,10 +52,34 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	private class StableArrayAdapter extends ArrayAdapter<String> {
 
-	    public StableArrayAdapter(Context context, int textViewResourceId) {
+
+	private class MyArrayAdapter extends ArrayAdapter<String> {
+
+	    public MyArrayAdapter(Context context, int textViewResourceId) {
 	      super(context, textViewResourceId);
 	    }
 	  }
+
+
+
+	@Override
+	public Loader<List<String>> onCreateLoader(int id, Bundle args) {
+		// TODO Auto-generated method stub
+		return new sshexec(this);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<List<String>> arg0, List<String> arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoaderReset(Loader<List<String>> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
