bed313c09b3d49cc5d17de9b24b47f4f37de4a4e
golubev <7886143+mustafirus@users.noreply.github.com>
2014-01-18 22:23:29 +0000

    revert to AsyncTask and Adapter

--- ServerTroll/src/ua/net/nt/servertroll/MainActivity.java
+++ ServerTroll/src/ua/net/nt/servertroll/MainActivity.java
@@ -6,72 +6,46 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.ContextWrapper;
-import android.database.Cursor;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.app.LoaderManager;
-import android.content.Loader;
 
-//import android.support.v4.app.LoaderManager.LoaderCallbacks;
-
-public class MainActivity extends Activity implements
-		LoaderManager.LoaderCallbacks<List<String>> {
-
-	ListView listview;
-	MyArrayAdapter adapter;
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		 final ListView listview = (ListView) findViewById(R.id.listView1);
+		    String[] values = new String[] { "Android1", "iPhone", "WindowsMobile",
+		        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+		        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+		        "Android", "iPhone", "WindowsMobile" };
 
-		listview = (ListView) findViewById(R.id.listView1);
-		adapter = new MyArrayAdapter(this, R.layout.my_list_item);
-		adapter.add("FuckYou");
+		    final StableArrayAdapter adapter = new StableArrayAdapter(this,
+		        R.layout.my_list_item);
+		    listview.setAdapter(adapter);
 
-		listview.setAdapter(adapter);
-		//setListAdapter(adapter);
-		LoaderManager.enableDebugLogging (true);
-		getLoaderManager().initLoader(0, null, this);
-
+		    new sshexec().execute(adapter);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		//ContextWrapper c = new ContextWrapper(this);
+		ContextWrapper c = new ContextWrapper(this);
 		Toast.makeText(this, getFilesDir().getPath(), Toast.LENGTH_LONG).show();
 		return true;
 	}
 
-	private class MyArrayAdapter extends ArrayAdapter<String> {
+	private class StableArrayAdapter extends ArrayAdapter<String> {
 
-		public MyArrayAdapter(Context context, int textViewResourceId) {
-			super(context, textViewResourceId);
-		}
-	}
-
-	@Override
-	public Loader<List<String>> onCreateLoader(int id, Bundle args) {
-		// TODO Auto-generated method stub
-		return new sshexec(this);
-	}
-
-	@Override
-	public void onLoadFinished(Loader<List<String>> loader, List<String> data) {
-		adapter.addAll(data);
-	}
-
-	@Override
-	public void onLoaderReset(Loader<List<String>> arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
+	    public StableArrayAdapter(Context context, int textViewResourceId) {
+	      super(context, textViewResourceId);
+	    }
+	  }
 }
