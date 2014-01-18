a5a11d509241c971c197cf794fc9356b1b8b3cf7
golubev <7886143+mustafirus@users.noreply.github.com>
2014-01-18 23:02:52 +0000

    working loading into Adapter  with AsynckTask

--- ServerTroll/src/ua/net/nt/servertroll/sshexec.java
+++ ServerTroll/src/ua/net/nt/servertroll/sshexec.java
@@ -1,26 +1,33 @@
 package ua.net.nt.servertroll;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
 import com.jcraft.jsch.*;
 import java.io.*;
 import java.util.ArrayList;
 
-public class sshexec extends AsyncTask<ArrayAdapter<String>, Void, Void>{
+public class sshexec extends AsyncTask<Void, String, Void>{
 
-	protected Void doInBackground(ArrayAdapter<String>...adapters) {
+	ArrayAdapter<String> adapter;
 
-		final ArrayList<String> list = new ArrayList<String>();
+	public sshexec(ArrayAdapter<String> adapter){
+		this.adapter = adapter;
+	}
+	
+	protected Void doInBackground(Void...v) {
+
+//		final ArrayList<String> list = new ArrayList<String>();
 		try {
 			JSch jsch = new JSch();
-
+			JSch.setConfig("StrictHostKeyChecking", "no");
+			
 			String user = "root";
 			String host = "mail";
 
 			Session session = jsch.getSession(user, host);
 
-			ArrayAdapter<String> adapter = adapters[0];
 			String home = adapter.getContext().getFilesDir().getPath();
 
 			jsch.setKnownHosts(home + "/.ssh/known_hosts");
@@ -31,7 +38,7 @@ public class sshexec extends AsyncTask<ArrayAdapter<String>, Void, Void>{
 			session.setPassword("cdvfbg1q");
 			session.connect();
 
-			String command = "ls";
+			String command = "cat /var/log/mail.info";
 			// command=args[1];
 
 			Channel channel = session.openChannel("exec");
@@ -49,17 +56,20 @@ public class sshexec extends AsyncTask<ArrayAdapter<String>, Void, Void>{
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			String line;
 			while ((line = br.readLine()) != null) {
-				adapter.add(line);
+				publishProgress (line);
+				//TODO: bulk UI updates
 			}
 
 			channel.disconnect();
 			session.disconnect();
 		} catch (Exception e) {
-			String err = e.toString();
-			String aaa = "aaa";
-			// System.out.println(e);
-		}
+			Log.e("sshexec", "Error", e);		}
 		return null;
 		
 	} // end main
+	@Override
+	protected void onProgressUpdate (String... values){
+		String line = values[0];
+		adapter.add(line);
+	}
 } // end class
